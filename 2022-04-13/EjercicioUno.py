7b386b3c04a118110964c2c5cb3599a371dc3428
RubenUcentral <67939628+RubenUcentral@users.noreply.github.com>
2022-04-12 20:36:23 -0400

    if - else

---
+++ 2022-04-13/EjercicioUno.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 12 20:34:35 2022
+
+@author: Rubenz
+"""
+
+#if - else
+
+variable = True
+
+if(variable):
+    print('Es verdadero')
+else: 
+    print('Es falso')
+
