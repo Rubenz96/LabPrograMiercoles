8b087f475beabdce25509cee476114a4556946b6
RubenUcentral <67939628+RubenUcentral@users.noreply.github.com>
2022-04-19 21:39:11 -0400

    Update EjercicioUno.py

--- 2022-04-13/EjercicioUno.py
+++ 2022-04-13/EjercicioUno.py
@@ -1,16 +1,72 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Apr 12 20:34:35 2022
+Created on Tue Apr 19 19:36:38 2022
 
 @author: Rubenz
 """
 
-#if - else
 
-variable = True
+# valor_uno = (int)(input('Ingrese el valor uno: '))
+# valor_dos = (int)(input('Ingrese el valor dos: '))
 
-if(variable):
-    print('Es verdadero')
-else: 
-    print('Es falso')
+# if(valor_uno > valor_dos):
+#   print('El valor uno es mayor.')
+# else:
+#   print('El valor uno no es mayor.')
+
+# valor_uno = (int)(input('Ingrese el valor uno: '))
+# valor_dos = (int)(input('Ingrese el valor dos: '))
+
+# if(valor_uno <= valor_dos):
+#   print('Primer bloque')
+#   print('Primer bloque')
+#   print('Primer bloque')
+# else:
+#   print('Segundo bloque')
+#   print('Segundo bloque')
+#   print('Segundo bloque')
+
+
+# condicional = False
+
+# if(condicional):
+#   print('Bloque if')
+
+
+# condicional = 6 < 5
+# print(condicional)
+
+# if(condicional):
+#   print('Bloque if')
+# elif(True):
+#   print('Bloque else')
+
+
+# valor = (int)(input('Ingresa el valor: '))
+
+# if(valor>0):
+#   print('El valor es mayor a cero')
+  
+#   if(valor<=6):
+#     print('El valor es mayor a cero, pero no por mucho')
+# else:
+#   print('El valor no es mayor a cero')
+  
+
+valor = (int)(input('Ingresa el valor: '))
+
+if(valor>0):
+  print('Es mayor a 0')
+# else:
+#   print('No es mayor a 0')
+
+if(valor>6):
+  print('Es mayor a 6')
+# else:
+#   print('No es mayor a 6')
+  
+if(valor>10):
+  print('Es mayor a 10')
+else:
+  print('No es mayor a 10')
 
