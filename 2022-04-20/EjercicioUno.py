c8a2603381dc7384ea3d8da5eea52d11f078a8aa
RubenUcentral <67939628+RubenUcentral@users.noreply.github.com>
2022-04-22 16:43:00 -0400

    Create EjercicioUno.py

---
+++ 2022-04-20/EjercicioUno.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 22 16:41:53 2022
+
+@author: Rubenz
+"""
+
+######################## OPE LOGICOS #######################
+
+# valor_uno = (int)(input('Ingrese un numero: '))
+# valor_dos = (int)(input('Ingrese un numero: '))
+
+# valor_tres = (int)(input('Ingrese un numero: '))
+# valor_cuatro = (int)(input('Ingrese un numero: '))
+
+###########################AND##########################
+# if(valor_uno==valor_dos and valor_tres > valor_cuatro):
+#   print('El valor uno es igual a dos, y el valor tres es mayor a cuatro')
+# else:
+#   print('Las condiciones no se cumplieron')
+
+###########################OR##########################
+
+# if(valor_uno==valor_dos or valor_tres > valor_cuatro):
+#   print('El valor uno es igual a dos, y el valor tres es mayor a cuatro')
+# else:
+#   print('Las condiciones no se cumplieron')
+
+###########################NOT##########################
+
+# condicion = 5 > 5
+# print(condicion)
+
+# if(not condicion):
+#   print('Entra en el if')
+# else:
+#   print('Entra en el else')
+
+########################### ELIF ##########################
+
+
+# valor_uno = (int)(input('Ingrese un numero: '))
+# valor_dos = (int)(input('Ingrese un numero: '))
+
+# if(valor_uno == valor_dos):
+#   print('Los valores son iguales')
+# elif(valor_uno > valor_dos):
+#   print('El valor uno es mayor')
+# elif(valor_uno < valor_dos):
+#   print('El valor dos es mayor')
+# else:
+#   print('No se cumplen las condiciones')
+
