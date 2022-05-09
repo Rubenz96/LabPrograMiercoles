42a14659668217ddb002d76f9e57248c5a0eec90
RubenUcentral <67939628+RubenUcentral@users.noreply.github.com>
2022-05-09 19:44:13 -0400

    Clases 27/04 y 04/05

---
+++ 2022-04-27/EjercicioUno.py
@@ -0,0 +1,59 @@
+
+cant = (int)(input('Ingrese cantidad de ciclos: '))
+
+for i in range(cant):
+  print(i)
+
+
+
+# for i in range(10,100):
+#   print(i)
+
+# ######################################
+# cantidad_notas = 6
+# sumatoria = 0
+
+# for i in range(cantidad_notas):
+#   nota = (float)(input('Ingrese una nota: '))
+#   sumatoria = sumatoria + nota 
+
+# # print(sumatoria)
+
+# promedio = sumatoria / cantidad_notas
+# print(promedio)
+
+######################################
+# cantidad_notas = 6
+# sumatoria = 0
+# quiere_promedio = (int)(input('Quiere promedio? 0 = no ; 1 = si'))
+# if(quiere_promedio==1):
+#   for i in range(cantidad_notas):
+#     nota = (float)(input('Ingrese una nota: '))
+#     sumatoria = sumatoria + nota 
+  
+#   # print(sumatoria)
+  
+#   promedio = sumatoria / cantidad_notas
+#   print(promedio)
+# else:
+#   print('No quiere promedio')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 2022-05-04/EjercicioUno.py
@@ -0,0 +1,69 @@
+
+
+# valor = (int)(input('Ingrese un numero: '))
+
+
+# while(valor<10):
+#   print('El valor es: ',valor)
+#   valor = (int)(input('Ingrese un nuevo numero: '))
+  
+##############################################
+# valor = (int)(input('Ingrese un numero: '))
+
+# while(valor!=6):
+#   print('El valor es: ',valor)
+#   valor = (int)(input('Ingrese un nuevo numero: '))
+
+
+# print('----------Fin----------')
+# print(valor)
+
+##############################################
+
+# cantidad_notas = (int)(input('Por favor ingrese la cantidad de notas: '))
+# sumador = 0
+# contador = 0
+
+# while(contador<cantidad_notas):
+#   nota = (float)(input('Ingrese la nota: '))
+#   sumador = sumador + nota
+#   contador = contador + 1
+
+# promedio = sumador /cantidad_notas
+# print(promedio)
+  
+#########################################
+
+sumador = 0
+contador = 1
+
+limite = (int)(input('Ingrese el límite: '))
+
+while(contador <= limite):
+
+  sumador = sumador + contador
+  
+  print('--------------------')
+  print(contador)
+  print(sumador)
+  contador = contador + 1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
